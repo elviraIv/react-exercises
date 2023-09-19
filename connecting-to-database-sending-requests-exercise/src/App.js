ae6dc5e66aa72ed3858fe7f450fcf004fa879290
Elvira <elvira.n.ivanova@gmail.com>
2023-09-19 11:10:33 +0300

    Added error handling.

--- connecting-to-database-sending-requests-exercise/src/App.js
+++ connecting-to-database-sending-requests-exercise/src/App.js
@@ -5,23 +5,47 @@ import "./App.css";
 
 function App() {
   const [movies, setMoives] = useState([]);
+  const [isLoadind, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   function fetchMovieHandler() {
+    setIsLoading(true);
+    setError(null);
     fetch("https://swapi.dev/api/films")
       .then((response) => {
         return response.json();
       })
       .then((data) => {
-        const transformedMovies = data.results.map(movieData => {
+        const transformedMovies = data.results.map((movieData) => {
           return {
             id: movieData.episode_id,
             title: movieData.title,
             releaseDate: movieData.release_date,
-            openingText: movieData.opening_crawl
-          }
-        })
+            openingText: movieData.opening_crawl,
+          };
+        });
         setMoives(transformedMovies);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        setError("Something went wrong!");
+        throw new Error(error.message);
       });
+  }
+
+  let content = <p>No movies found.</p>
+
+  if(isLoadind){
+    content = <p>Loading...</p>
+  }
+
+  if(movies.length > 0){
+    content = <MoviesList movies={movies} />
+  }
+
+  if (error){
+    content = <p>{error}</p>
   }
 
   return (
@@ -30,7 +54,7 @@ function App() {
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {content}
       </section>
     </React.Fragment>
   );
