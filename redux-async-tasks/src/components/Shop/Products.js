348b227333d1c23d3a11f31f36819735c556b416
Elvira <elvira.n.ivanova@gmail.com>
2023-09-24 15:44:04 +0300

    Added cartSlice and uiSlice reducers.

--- redux-async-tasks/src/components/Shop/Products.js
+++ redux-async-tasks/src/components/Shop/Products.js
@@ -1,16 +1,41 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: "6",
+    title: "first book",
+    description: "the first I  ever wrote!",
+  },
+  {
+    id: "p2",
+    price: "4",
+    title: "second book",
+    description: "the second I ever wrote!",
+  },
+  {
+    id: "p3",
+    price: "3",
+    title: "third book",
+    description: "the third I  ever wrote!",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
