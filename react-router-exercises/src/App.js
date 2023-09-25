8e3874485033792b181b6c27925122b87a64e34c
Elvira <elvira.n.ivanova@gmail.com>
2023-09-25 16:59:32 +0300

    Added initial code.

--- react-router-exercises/src/App.js
+++ react-router-exercises/src/App.js
@@ -1,15 +1,29 @@
-import { Route } from "react-router-dom/cjs/react-router-dom.min";
+import { Route, Switch } from "react-router-dom";
 import { Welcome } from "./pages/Welcome";
 import { Products } from "./pages/Products";
+import { MainHeader } from "./components/MainHeader";
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
+
 function App() {
   return (
     <div>
-      <Route path="/welcome">
-        <Welcome/>
-      </Route>
-      <Route path="/products">
-        <Products />
-      </Route>
+      <header>
+        <MainHeader />
+      </header>
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome"/>
+          </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="product/:productId"></Route>
+        </Switch>
+      </main>
     </div>
   );
 }
