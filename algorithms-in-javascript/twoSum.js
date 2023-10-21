5eb87c5e591ae2e4d6ba37e6c1bc845db3c43694
Elvira <elvira.n.ivanova@gmail.com>
2023-10-21 20:27:20 +0300

    Added twoSum function

---
+++ algorithms-in-javascript/twoSum.js
@@ -0,0 +1,9 @@
+//return every pair of array that adds up to the given sum
+
+//result should be array of arrays
+// it is okay one number to be used in several pairs
+
+function twoSum(numArray, sum){
+
+}
+
