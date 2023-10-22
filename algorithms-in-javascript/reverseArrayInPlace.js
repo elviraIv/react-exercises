7a9fac9b3936500513573e20100c3108340468c2
Elvira <elvira.n.ivanova@gmail.com>
2023-10-22 23:33:18 +0300

    Added logic

--- algorithms-in-javascript/reverseArrayInPlace.js
+++ algorithms-in-javascript/reverseArrayInPlace.js
@@ -1,7 +1,13 @@
 // Reverse an array: by manipulating, and not using .reverse()
 
 function reverseArrayInPlace(array){
-
+    
+    for (let index = 0; index < array.length; index++) {
+        let temp = array[index]
+        
+        
+    }
 }
 
-reverseArrayInPlace()
+
+reverseArrayInPlace();
