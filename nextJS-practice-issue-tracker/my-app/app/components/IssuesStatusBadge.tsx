bd9ea21c3bf121e373cdce55ba99d05cacbbf7f6
Elvira <elvira.n.ivanova@gmail.com>
2023-10-26 13:20:54 +0300

    Add issue status badge

---
+++ nextJS-practice-issue-tracker/my-app/app/components/IssuesStatusBadge.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Status } from '@prisma/client'
+import { Badge, Flex } from '@radix-ui/themes'
+
+const statusMap: Record<Status, {label:string, color:'red' | 'violet' | 'green'}> = {
+    OPEN:{
+        label: 'Open',
+        color: 'red'
+    },
+    IN_PROGRESS:{
+        label: 'In Progress',
+        color: 'violet'
+    },
+    CLOSED: {
+        label: 'Closed',
+        color: 'green'
+    }
+}
+
+
+const IssuesStatusBadge = ({status} : {status: Status}) => {
+  return (
+    <Badge color={statusMap[status].color}>{statusMap[status].label}</Badge>
+    )
+}
+
+export default IssuesStatusBadge
--- nextJS-practice-issue-tracker/my-app/app/issues/page.tsx
+++ nextJS-practice-issue-tracker/my-app/app/issues/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
 import prisma from "@/prisma/client";
+import IssuesStatusBadge from "../components/IssuesStatusBadge";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
@@ -31,9 +32,15 @@ const IssuesPage = async () => {
             <Table.Row key={issue.id}>
                <Table.Cell>
                 {issue.title}
-                <div className='block md:hidden'>{issue.status}</div>
+                <div className="block md:hidden">
+                  <IssuesStatusBadge status={issue.status} />
+                </div>
+                
+                </Table.Cell>
+              <Table.Cell className='hidden md:table-cell'>
+                <IssuesStatusBadge status={issue.status} />
+
               </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">{issue.status}</Table.Cell>
               <Table.Cell className="hidden md:table-cell">{issue.createdAt.toDateString()}</Table.Cell>
             </Table.Row>
           ))}
