97ebc4cae9e86e86154118e3db6dc23bc6ac57f7
Elvira <elvira.n.ivanova@gmail.com>
2023-10-28 11:20:07 +0300

    Build EditIssue page

--- nextJS-practice-issue-tracker/my-app/app/issues/new/page.tsx
+++ nextJS-practice-issue-tracker/my-app/app/issues/new/page.tsx
@@ -1,74 +1,11 @@
-"use client";
+import IssueForm from "../_components/IssueForm";
 
-import React, { useState } from "react";
-import { TextField, Button, Callout, Text } from "@radix-ui/themes";
-import { useForm, Controller } from "react-hook-form";
-import axios from "axios";
-import "easymde/dist/easymde.min.css";
-import { useRouter } from "next/navigation";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { createIssueSchema } from "@/app/validationSchemas.ts";
-import { z } from "zod";
-import ErrorMessage from "@/app/components/ErrorMessage";
-import Spinner from "@/app/components/Spinner";
-import dynamic from "next/dynamic";
-
-const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
-  ssr:false
-})
-
-type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
-  const router = useRouter();
-  const {
-    register,
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<IssueForm>({
-    resolver: zodResolver(createIssueSchema),
-  });
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-
-  const onSubmit = handleSubmit(async (data) => {
-    try {
-      setIsLoading(true);
-      await axios.post("/api/issues", data);
-      router.push("/issues");
-    } catch (error) {
-      setIsLoading(false);
-      setError("An unexpected error occured.");
-    }
-  });
+ 
 
   return (
-    <div className="max-w-xl">
-      {error && (
-        <Callout.Root color="red" className="mb-5">
-          <Callout.Text>{error}</Callout.Text>
-        </Callout.Root>
-      )}
-      <form className=" space-y-3" onSubmit={onSubmit}>
-        <TextField.Root>
-          <TextField.Input placeholder="Title" {...register("title")} />
-        </TextField.Root>
-        <ErrorMessage>{errors.title?.message}</ErrorMessage>
-        <Controller
-          name="description"
-          control={control}
-          render={({ field }) => (
-            <SimpleMDE placeholder="Describe your problem…" {...field} />
-          )}
-        />
-        <ErrorMessage>{errors.description?.message}</ErrorMessage>
-        <Button disabled={isLoading}>
-          Submit New Issue
-          {isLoading && <Spinner />}
-        </Button>
-      </form>
-    </div>
+    <IssueForm />
   );
 };
 
