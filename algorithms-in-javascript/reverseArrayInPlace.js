f5b289366556cc4300ff05b67a0e1aa80a052466
Elvira <elvira.n.ivanova@gmail.com>
2023-11-22 21:54:45 +0200

    Add solution

--- algorithms-in-javascript/reverseArrayInPlace.js
+++ algorithms-in-javascript/reverseArrayInPlace.js
@@ -1,13 +1,13 @@
 // Reverse an array: by manipulating, and not using .reverse()
+//do not: manipulate the given array, nor push elements into a new array and return it
 
-function reverseArrayInPlace(array){
-    
-    for (let index = 0; index < array.length; index++) {
-        let temp = array[index]
-        
-        
-    }
+function reverseArrayInPlace(array) {
+  for (let index = 0; index < array.length / 2; index++) {
+    let temp = array[index];
+    array[index] = array[array.length - 1 - index];
+    array[array.length - 1 - index] = temp;
+  }
+  return array;
 }
 
-
-reverseArrayInPlace();
+console.log(reverseArrayInPlace([1,2,3,4,5,6,7,8,9]));
