fb6f9ed371a44a366542062a441c418599ab5788
Elvira <elvira.n.ivanova@gmail.com>
2023-11-23 08:37:46 +0200

    Add algorithm

--- algorithms-in-javascript/twoSum.js
+++ algorithms-in-javascript/twoSum.js
@@ -3,7 +3,22 @@
 //result should be array of arrays
 // it is okay one number to be used in several pairs
 
-function twoSum(numArray, sum){
+function twoSum(numArray, sum) {
+  let pairs = [];
+  let hashtable = [];
 
+  for (let i = 0; i < numArray.length; i++) {
+    let currNum = numArray[i];
+    let counterPart = sum - currNum;
+    // checking hashtable if needed counterPart exists
+    if (hashtable.indexOf(counterPart) !== -1) {
+      pairs.push([currNum, counterPart]);
+    }
+    hashtable.push(currNum);
+  }
+
+  return pairs;
 }
 
+// console.log(twoSum([1, 6, 4, 5, 3, 3], 7));
+console.log(twoSum([40,11,19,17,-12], 28));
